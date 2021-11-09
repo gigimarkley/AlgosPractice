cfc53b319f094aa7ef309e1f7a6107881e9ed1a0
Gillian (Gigi) Markley <86491127+gigimarkley@users.noreply.github.com>
2021-11-09 07:33:17 -0800

    Create MultiplicativePersistence.js

---
+++ MultiplicativePersistence.js
@@ -0,0 +1,21 @@
+//Given non-negative integer
+//return number of times to multiple digits until it becomes a single digit
+
+//time: O(n)
+//space: O(n)
+function persistence (num) {
+  let product = 1
+  let digitsArr = Array.from(String(num), Number)
+  if (digitsArr.length === 1) {
+    return 0
+  } else {
+    for (let i=0;i<digitsArr.length;i++) {
+    product = product * digitsArr[i]
+    }
+    return persistence(product) + 1
+  }
+}
+
+console.log(persistence(39));  // 3 
+console.log(persistence(111)); // 1
+console.log(persistence(1));   // 0
